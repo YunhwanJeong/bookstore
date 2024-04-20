1adcfaaa3670f80946847f08410f9b557eb04d86
yunhwan <jimmy53120488@gmail.com>
2024-04-20 16:57:43 -0400

    feat: add HTML serving code in server.js to support SSR

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 import express from 'express';
+import fs from 'node:fs/promises';
 
 // Constants
 const isProduction = process.env.NODE_ENV === 'production';
 const port = process.env.PORT || 5173;
 const base = process.env.BASE || '/';
+
+// Cached production assets
+const templateHtml = isProduction ? await fs.readFile('./dist/client/index.html', 'utf-8') : '';
+const ssrManifest = isProduction ? await fs.readFile('./dist/client/.vite/ssr-manifest.json', 'utf-8') : undefined;
 
 // Create http server
 const app = express();
@@ -26,7 +31,35 @@ if (!isProduction) {
 }
 
 // Serve HTML
-app.use('*', async (req, res) => {});
+app.use('*', async (req, res) => {
+  try {
+    const url = req.originalUrl.replace(base, '');
+
+    let template;
+    let render;
+    if (!isProduction) {
+      // Always read fresh template in development
+      template = await fs.readFile('./index.html', 'utf-8');
+      template = await vite.transformIndexHtml(url, template);
+      render = (await vite.ssrLoadModule('/src/entry-server.tsx')).render;
+    } else {
+      template = templateHtml;
+      render = (await import('./dist/server/entry-server.js')).render;
+    }
+
+    const rendered = await render(url, ssrManifest);
+
+    const html = template
+      .replace(`<!--app-head-->`, rendered.head ?? '')
+      .replace(`<!--app-html-->`, rendered.html ?? '');
+
+    res.status(200).set({ 'Content-Type': 'text/html' }).send(html);
+  } catch (e) {
+    vite?.ssrFixStacktrace(e);
+    console.log(e.stack);
+    res.status(500).end(e.stack);
+  }
+});
 
 // Start http server
 app.listen(port, () => {
