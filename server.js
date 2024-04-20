2f32b44dd80a9c5f8b17cf62f386a887a0332f57
yunhwan <jimmy53120488@gmail.com>
2024-04-20 16:05:23 -0400

    feat: add initial http server structure using express in server.js

---
+++ server.js
@@ -0,0 +1,34 @@
+import express from 'express';
+
+// Constants
+const isProduction = process.env.NODE_ENV === 'production';
+const port = process.env.PORT || 5173;
+const base = process.env.BASE || '/';
+
+// Create http server
+const app = express();
+
+// Add Vite or respective production middlewares
+let vite;
+if (!isProduction) {
+  const { createServer } = await import('vite');
+  vite = await createServer({
+    server: { middlewareMode: true },
+    appType: 'custom',
+    base,
+  });
+  app.use(vite.middlewares);
+} else {
+  const compression = (await import('compression')).default;
+  const sirv = (await import('sirv')).default;
+  app.use(compression());
+  app.use(base, sirv('./dist/client', { extensions: [] }));
+}
+
+// Serve HTML
+app.use('*', async (req, res) => {});
+
+// Start http server
+app.listen(port, () => {
+  console.log(`Server started at http://localhost:${port}`);
+});
