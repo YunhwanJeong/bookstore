2e5f1d0a504cef6b7424252ee924e72f78bde586
yunhwan <jimmy53120488@gmail.com>
2024-04-20 15:29:52 -0400

    feat: add entry-client and placeholder in the index.html

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.hydrateRoot(
+  document.getElementById('root') as HTMLElement,
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
