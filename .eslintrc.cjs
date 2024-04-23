7545b9f0ab4e899ab6373a33f14084218868b485
yunhwan <jimmy53120488@gmail.com>
2024-04-23 16:08:07 -0400

    config: add eslint rules for pre-typed redux hooks

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,10 +7,24 @@ module.exports = {
     'plugin:react-hooks/recommended',
     'prettier',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'vite.config.ts'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: { project: true, tsconfigRootDir: './' },
+  plugins: ['react-refresh', '@typescript-eslint'],
   rules: {
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    '@typescript-eslint/consistent-type-imports': [2, { fixStyle: 'separate-type-imports' }],
+    '@typescript-eslint/no-restricted-imports': [
+      2,
+      {
+        paths: [
+          {
+            name: 'react-redux',
+            importNames: ['useSelector', 'useStore', 'useDispatch'],
+            message: 'Please use pre-typed versions from `src/app/hooks.ts` instead.',
+          },
+        ],
+      },
+    ],
   },
 };
